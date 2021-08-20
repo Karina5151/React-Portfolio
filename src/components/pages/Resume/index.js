274c4abb4955e39257cb633f0f53698dd46a418b
Nazala <nazala5151@gmail.com>
2021-08-20 08:16:17 -0500

    adding skills

--- src/components/pages/Resume/index.js
+++ src/components/pages/Resume/index.js
@@ -1,6 +1,20 @@
 export default function Resume() {
     return (
-        <>
-        </>
+        <div className="resume col-12-lg col-6-md col-3-sm text-center">
+            <h1>Learned Technologies</h1>
+            <div className="skillContainer ">
+                <li className="skill"><a href="https://www.w3schools.com/html/default.asp" target="_blank" rel="noreferrer"></a></li>
+                <li className="skill"><a href="https://www.w3schools.com/css/default.asp" target="_blank" rel="noreferrer"></a></li>
+                <li className="skill"><a href="https://www.w3schools.com/js/default.asp" target="_blank" rel="noreferrer"></a></li>
+                <li className="skill"><a href="https://nodejs.org/en/docs/" target="_blank" rel="noreferrer"></a></li>
+                <br></br>
+                <li className="skill"><a href="https://dev.mysql.com/doc/" target="_blank" rel="noreferrer"></a></li>
+                <li className="skill"><a href="https://www.mongodb.com/" target="_blank" rel="noreferrer"></a></li>
+                <li className="skill"><a href="https://reactjs.org/" target="_blank" rel="noreferrer"></a></li>
+                <li className="skill"><a href="https://graphql.org/" target="_blank" rel="noreferrer"></a></li>
+                <br></br>
+                <a href="" target="_blank" rel="noreferrer">Link to Resume on Google Drive</a>
+            </div>
+        </div>
     );
 }
