51331c06a6bbd2a4114e3477137ffe9df36eef19
Nazala <nazala5151@gmail.com>
2021-10-18 15:09:00 -0500

    footer quote and resume links complete

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,12 +6,7 @@ export default function Footer() {
     return (
         <footer className="footer" style={{ backgroundImage: `url(${backgroundImg})`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundPosition: 'center'}}>
             <div className="footer-container text-center">
-                <a href="https://github.com/Karina5151" target="_blank" rel="noreferrer">
-                    My Github Page
-                </a>
-                <a href="https://www.linkedin.com/in/karina-clausen-5049aaa9/" target="_blank" rel="noreferrer">
-                    My LinkedIn Page
-                </a>
+                <p>"Love challenges, be intrigued by mistakes, enjoy effort, and keep on learning" - Carol Dweck</p>
             </div>
         </footer>
     );
